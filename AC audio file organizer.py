9d8a49d9ac2d3f566e12b6e30eb5f4ff9b5181d9
Andrei Segal <andreisegal245@gmail.com>
2021-12-28 18:38:41 -0300

    used .bat workaround. Yeah

--- AC audio file organizer.py
+++ AC audio file organizer.py
@@ -1,8 +1,8 @@
 ## AC Audio File Organizer - by Andreisgl @ Github
 # Manipulates the folder with audio files in order to organize each file to aid editing.
 
+import os
 import subprocess
-
 
 
 
@@ -19,32 +19,49 @@ import subprocess
 #8 -  "InputFile"	Input file to play/convert
 #9 -  "OutputFile"	Output file to convert to
 
-numArguments = 10
 
-inputFilename = 'lol.npsf'
-outputFilename = 'lol.wav'
+
+
+
+
+manipulateMode = 0 # Mode for 'manipulateFile' function.
+                        # 0: Convert only
+                        # 1: Reproduce only
 aczRADIOArguValues = [22050, 1, 320, 0, 'WAVU', 22050, 1, 320] # Temporary. Argument value set for ACZ RADIOUSA files.
 
-ARGU = ['/IF', '/IC', '/II', '/IH', '/OT', '/OF', '/OC', '/OI'] #Contains the core arguments.
-
-currentArguValue = [] #The current set of argument VALUES currently being used.
-
-concatArgu = [] #Concatenated Arguments and values
 
 
-argumentBuffer = ''
+def manipulateFile(inputFilename, outputFilename, arguValues, mode):
+    numArguments = 10
+    exeFilename = 'MFAudio.exe'
 
-currentArguValue = aczRADIOArguValues
+    batFilename = 'temp.bat' # Temporary .bat file to execute MFAudio.exe (It only worked when I did this...)
+
+    ARGU = ['/IF', '/IC', '/II', '/IH', '/OT', '/OF', '/OC', '/OI'] #Contains the core arguments.
+    currentArguValue = [] #The current set of argument VALUES currently being used.
+    concatArgu = [] #Concatenated Arguments and values
+    argumentBuffer = ''
+    currentArguValue = arguValues
 
 
-for i in range(len(ARGU)):
-    concatArgu.append( ARGU[i] + str(currentArguValue[i]) )
+    for i in range(len(ARGU)): # Concatenates each core parameter with it's value individually, by list index.
+        concatArgu.append( ARGU[i] + str(currentArguValue[i]) )
 
-for i in range(len(ARGU)):
-    argumentBuffer = argumentBuffer + concatArgu[i] + ' '
+    for i in range(len(ARGU)): # Creates string based on 'concatArgu' list contents.
+        argumentBuffer = argumentBuffer + concatArgu[i] + ' '
 
-argumentBuffer = argumentBuffer + '"' + inputFilename + '"' + ' ' + '"' + outputFilename + '"'
+    argumentBuffer = exeFilename + ' ' + argumentBuffer + '"' + inputFilename + '"' # Adsa input filename to 'argumentBuffer' string and...
+    
+
+    if mode == 0: # ... if the mode is "Convert (0)", add output filename as well.
+        argumentBuffer = argumentBuffer + ' ' + '"' + outputFilename + '"'
+    
+    with open(batFilename, 'w') as batFile:
+        batFile.write(argumentBuffer)
+        
+    subprocess.run(batFilename)
+    os.remove(batFilename)
 
 
-subprocess.run(["MFAudio.exe", argumentBuffer], shell=True)
-#subprocess.run( ["MFAudio.exe", '/IF22050 /IC1 /II320 /IH0 /OTWAVU /OF22050 /OC1 /OI320', '"lol.npsf"', '"lol.wav"'], shell=True )
+
+manipulateFile('lol.npsf', 'lol.wav', aczRADIOArguValues, 0)
