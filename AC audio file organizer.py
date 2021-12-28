6d16a919ef9c280b60fc7f157f008f8a2f707256
Andrei Segal <andreisegal245@gmail.com>
2021-12-28 19:12:28 -0300

    update comments

--- AC audio file organizer.py
+++ AC audio file organizer.py
@@ -25,17 +25,17 @@ def manipulateFile(inputFilename, outputFilename, arguValues, mode):
             # mode == 1: Reproduce audio
 
     # Argument list for MFAudio, with indexes for the lists used in this code
-    #0 -  /IFnnnnn	Input frequency
-    #1 -  /ICn	Input channels
-    #2 -  /IIxxxx	Input interleave (hex)
-    #3 -  /IHxxxx	Input headerskip (hex)
-    #4 -  /OTtttt	Output type (WAVU, VAGC,
-    # 	            SS2U, SS2C, RAWU, RAWC)
-    #5 -  /OFnnnnn	Output frequency
-    #6 -  /OCn	Output channels
-    #7 -  /OIxxxx	Output interleave (hex)
-    #8 -  "InputFile"	Input file to play/convert
-    #9 -  "OutputFile"	Output file to convert to
+        #0 -  /IFnnnnn	Input frequency
+        #1 -  /ICn	Input channels
+        #2 -  /IIxxxx	Input interleave (hex)
+        #3 -  /IHxxxx	Input headerskip (hex)
+        #4 -  /OTtttt	Output type (WAVU, VAGC,
+        # 	            SS2U, SS2C, RAWU, RAWC)
+        #5 -  /OFnnnnn	Output frequency
+        #6 -  /OCn	Output channels
+        #7 -  /OIxxxx	Output interleave (hex)
+        #8 -  "InputFile"	Input file to play/convert
+        #9 -  "OutputFile"	Output file to convert to
 
     exeFilename = 'MFAudio.exe'
 
