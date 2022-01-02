0ef116cd2be5a9d16578547c67a1416fd201d8e4
Andrei Segal <andreisegal245@gmail.com>
2022-01-02 18:29:18 -0300

    remove .bat workaround

--- AC audio file organizer.py
+++ AC audio file organizer.py
@@ -65,8 +65,6 @@ def manipulate_file(input_filename, output_filename, arg_values, mode):
 
     exe_filename = 'MFAudio.exe'
 
-    bat_filename = 'temp.bat' # Temporary .bat file to execute MFAudio.exe (It only worked when I did this...)
-
     DESTINATION_PATH = BASE_DIRECTORY
 
     args = ['/IF', '/IC', '/II', '/IH', '/OT', '/OF', '/OC', '/OI'] #Contains the core arguments.
@@ -82,16 +80,12 @@ def manipulate_file(input_filename, output_filename, arg_values, mode):
         argument_buffer = argument_buffer + concat_args[i] + ' '
 
     # Adds a input filename to 'argumentBuffer' string and...
-    argument_buffer = exe_filename + argument_buffer + '"' + DESTINATION_PATH + '\\'+ input_filename + '"'
+    argument_buffer = exe_filename + ' ' + argument_buffer + '"' + DESTINATION_PATH + '\\'+ input_filename + '"'
 
     if mode == 0:  # ... if the mode is "Convert (0)", add output filename as well.
         argument_buffer = argument_buffer + ' ' + '"' + output_filename + '"'
 
-    with open(bat_filename, 'w') as bat_file: # Create .bat file
-        bat_file.write(argument_buffer)
-
-    subprocess.run([bat_filename])
-    os.remove(bat_filename)
+    subprocess.run(argument_buffer)
 
 
 
