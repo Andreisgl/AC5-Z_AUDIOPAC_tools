a2ebf7893228d1c8d8c3bd04765d27a1aa1e52ea
Andrei Segal <andreisegal245@gmail.com>
2022-05-03 19:41:22 +0300

    will process .PAC files in bulk

--- ACZ PAC parser.py
+++ ACZ PAC parser.py
@@ -4,40 +4,62 @@
 ##===============================================================
 
 import os
+from os import listdir
+from os.path import isfile, join
+
+import pathlib
 import shutil
 import textwrap
 
-ORIGINAL_FILE_NAME = "DATA.PAC" # Change this for fast debugging
-RESULTING_FILE_NAME = "DATA_TBL.acd"
-
-WORK_FOLDER = "./WORK"
 
 
-ORIGINAL_FILE_NAME = WORK_FOLDER + "/" + ORIGINAL_FILE_NAME
-RESULTING_FILE_NAME = WORK_FOLDER + "/" + RESULTING_FILE_NAME
+
 
 def write_tbl_from_pac():
     print(textwrap.fill("///INFO///: Processing files, please wait...", width=72))
 
     
 
-    if not os.path.exists(WORK_FOLDER):
-            os.mkdir(WORK_FOLDER)
-    
-    offset_list = []
-    
-    try:
-        tbl_size = os.path.getsize(ORIGINAL_FILE_NAME)
+    BASE_FOLDER = "./WORK"
+    INPUT_FOLDER = BASE_FOLDER + "/" + "IN"
+    OUTPUT_FOLDER = BASE_FOLDER + "/" + "OUT"
+
+    if not os.path.exists(BASE_FOLDER):
+            os.mkdir(BASE_FOLDER)
+    if not os.path.exists(INPUT_FOLDER):
+            os.mkdir(INPUT_FOLDER)
+    if not os.path.exists(OUTPUT_FOLDER):
+            os.mkdir(OUTPUT_FOLDER)
+
+    in_file_list = listdir(INPUT_FOLDER)
+    for i in range(-len(in_file_list), 0):
+        if not isfile(INPUT_FOLDER + "/" + in_file_list[i]):
+            in_file_list.pop(i)
+        if not pathlib.Path(INPUT_FOLDER + "/" + in_file_list[i]).suffix == ".PAC":
+            in_file_list.pop(i)
+
+    #[f for f in listdir(BASE_FOLDER) if isfile(join(BASE_FOLDER, f))]
+
+    for i in range(len(in_file_list)):
+        ORIGINAL_FILE_NAME = in_file_list[i] # Change this for fast debugging
+        RESULTING_FILE_NAME = pathlib.Path(in_file_list[i]).stem + "_TBL.acd"
+        ORIGINAL_FILE_NAME = INPUT_FOLDER + "/" + ORIGINAL_FILE_NAME
+        RESULTING_FILE_NAME = OUTPUT_FOLDER + "/" + RESULTING_FILE_NAME
+
+        offset_list = []
         
-    except FileNotFoundError as x:
-        print ()
-        errormsg = "///ERROR///: .PAC file not found."
-        print (errormsg)
-        print ()
-        input("///INPUT///: Press any key to exit...")
-        exit(errormsg)
-    
-    with open(ORIGINAL_FILE_NAME, 'rb') as pac_file:
+        try:
+            tbl_size = os.path.getsize(ORIGINAL_FILE_NAME)
+            
+        except FileNotFoundError as x:
+            print ()
+            errormsg = "///ERROR///: .PAC file not found."
+            print (errormsg)
+            print ()
+            input("///INPUT///: Press any key to exit...")
+            exit(errormsg)
+        
+        with open(ORIGINAL_FILE_NAME, 'rb') as pac_file:
             for bytes_offset in range(0, tbl_size, 4):
                 data = pac_file.read(4)
                 if not data:
@@ -45,10 +67,10 @@ def write_tbl_from_pac():
                 if data == b'NPSF':
                     offset_list.append(bytes_offset)
 
-    with open(RESULTING_FILE_NAME, "wb") as tbl_file:
-        tbl_file.write(len(offset_list).to_bytes(byteorder="little", length=4))
-        for element in offset_list:
-            tbl_file.write(element.to_bytes(4, byteorder="little"))
+        with open(RESULTING_FILE_NAME, "wb") as tbl_file:
+            tbl_file.write(len(offset_list).to_bytes(byteorder="little", length=4))
+            for element in offset_list:
+                tbl_file.write(element.to_bytes(4, byteorder="little"))
 
 
         
