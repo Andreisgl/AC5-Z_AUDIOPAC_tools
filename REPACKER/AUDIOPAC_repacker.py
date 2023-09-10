30d94581d9e5487cd68add3c337f3237b5bf9fc0
Andrei <46579508+Andreisgl@users.noreply.github.com>
2023-09-10 17:52:19 -0300

    Create AUDIOPAC_repacker.py
    squash

---
+++ REPACKER/AUDIOPAC_repacker.py
@@ -0,0 +1,12 @@
+## AUDIOPAC_repacker by Truc3 (Andreisgl @ Github, @truc3_8492 @ Twitter)
+## Aid by Luis Filipe Sales (luisfilipels @ GitHub)
+## Based on death_the_d0g's (deaththed0g @ Github, death_the_d0g @ Twitter)
+## original "BGMPAC" sripts for Ace Combat Zero
+##===============================================================
+
+# This script repacks extracted .npsf files into a single .PAC file
+
+import os
+import shutil
+
+from .. import presets
